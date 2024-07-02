690e259b366ae89da3b2f3baeef8b541f9eac3e8
ashishbhoure <ashishbhoure@gmail.com>
2024-07-02 23:41:12 +0530

    setup and ad loginand signup

---
+++ middleware.ts
@@ -0,0 +1,20 @@
+
+import { authMiddleware } from "@clerk/nextjs/server";
+
+export default authMiddleware();
+
+export const config = {
+    matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
+  };
+  
+ 
+ 
+ 
+ 
+  // import { clerkMiddleware } from "@clerk/nextjs/server";
+  
+  // export default clerkMiddleware();
+  
+  // export const config = {
+  //   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+  // };
