35c8ac394ee03466fa9007baa268d3fd02e41ea9
ashishbhoure <ashishbhoure@gmail.com>
2024-07-06 20:18:02 +0530

    transation form and add image feature and connect with cloudanary

--- app/(root)/transformations/add/[type]/page.tsx
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import Header from "@/components/shared/Header";
+import TransformationForm from "@/components/shared/TransformationForm";
+import React from "react";
+import { transformationTypes } from "@/constants";
+import { auth } from "@clerk/nextjs/server";
+import { getUserById } from "@/lib/actions/user.actions";
+import { redirect } from "next/navigation";
 
-const AddTransformationTypePage = () => {
+const AddTransformationTypePage = async ({
+  params: { type },
+}: SearchParamProps) => {
+  const transformation = transformationTypes[type];
+  const { userId } = auth();
+
+  if (!userId) redirect("/sign-in");
+
+  const user = await getUserById(userId);
+
   return (
-    <div>AddTransformationTypePage</div>
-  )
-}
+    <>
+      <Header title={transformation.title} subTitle={transformation.subTitle} />
 
-export default AddTransformationTypePage
+      <section className = "mt-10">
+        <TransformationForm
+          action="Add"
+          userId={user._id}
+          type={transformation.type as TransformationTypeKey}
+          creditBalance={user.creditBalance}
+        />
+      </section>
+     
+    </>
+  );
+};
+
+export default AddTransformationTypePage;
